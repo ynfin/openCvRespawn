9d6d5e3b4bd9905f209618d35b3e9f5bd3439118
ynfin <yngve@MacBook-Pro.local>
2017-07-03 19:21:58 +0200

    Initial Commit

---
+++ openCvRespawn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openCvRespawn
+//
+//  Created by Yngve Finnestad on 03/07/2017.
+//  Copyright © 2017 Yngve Finnestad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
